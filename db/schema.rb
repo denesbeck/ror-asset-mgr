90dc0943757801d7c621edb7480e5604faa31890
Denes Beck <denes.beck1@outlook.com>
2023-10-28 21:55:59 +0200

    package updates

--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,6 @@ ActiveRecord::Schema.define(version: 2022_11_02_130503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "assets", force: :cascade do |t|
-    t.string "name"
-    t.string "user_serial"
-    t.string "email"
-    t.string "country"
-    t.string "asset_serial"
-    t.string "asset_type"
-    t.date "expiration_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "owners", force: :cascade do |t|
     t.string "name"
     t.string "serial"
